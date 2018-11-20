6f1c934beb277fa0233c5b78f79551b743d542eb
samay-v <samay12231998@gmail.com>
2018-11-20 11:28:20 +0530

    made sma into a function and called it

--- sma.py
+++ sma.py
@@ -2,13 +2,19 @@ import matplotlib.pyplot as plt
 import quandl
 import pandas as pd
 
-#get data Indiabulls price 
-data=quandl.get("NSE/IBULISL", authtoken="aCGwrfdPYgxEJqdd3B6q", start_date="2018-09-10", end_date="2018-11-10")
+def SMA(sym, startDate, endDate):
+	qcode="NSE/"+sym
+	#get data Indiabulls price 
+	data=quandl.get(qcode, authtoken="aCGwrfdPYgxEJqdd3B6q", start_date=startDate, end_date=endDate)
 
-#calculate moving average using pandas
-moving_avg = data.rolling(5).mean()
-print moving_avg
+	#calculate moving average using pandas
+	moving_avg = data.rolling(5).mean()
+	print moving_avg
 
-#plot the moving average on a graph
-plt.plot(moving_avg)
-plt.show()
+	#plot the moving average on a graph
+	plt.plot(moving_avg)
+	plt.show()
+	return [expression]
+
+#call OCHL with sample data
+SMA("INFY", "2014-09-10", "2018-11-10")
